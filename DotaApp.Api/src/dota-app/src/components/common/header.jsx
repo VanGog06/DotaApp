5b5f360d4e48e39eab5f3239222ce7ac04361bc8
Georgi Stankov <vangog06406@gmail.com>
2019-08-16 22:07:20 +0300

    Created the header of the application

---
+++ DotaApp.Api/src/dota-app/src/components/common/header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+import { IndexLinkContainer } from 'react-router-bootstrap'
+
+const Header = () => (
+  <Navbar bg="dark" variant="dark">
+    <IndexLinkContainer to='/'>
+      <Navbar.Brand href="/">
+        {`<DotaApp />`}
+      </Navbar.Brand>
+    </IndexLinkContainer>
+
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <IndexLinkContainer to='/'>
+          <Nav.Link>Home</Nav.Link>
+        </IndexLinkContainer>
+
+        <IndexLinkContainer to='/login'>
+          <Nav.Link>Login</Nav.Link>
+        </IndexLinkContainer>
+
+        <IndexLinkContainer to='/register'>
+          <Nav.Link>Register</Nav.Link>
+        </IndexLinkContainer>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export default Header;
