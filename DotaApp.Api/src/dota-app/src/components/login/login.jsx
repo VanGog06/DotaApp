c26c33cd842782158392950f0cc5daa12a181352
Georgi Stankov <vangog06406@gmail.com>
2019-08-17 22:50:16 +0300

    Created the login page

--- DotaApp.Api/src/dota-app/src/components/login/login.jsx
+++ DotaApp.Api/src/dota-app/src/components/login/login.jsx
@@ -1,40 +1,104 @@
 import React from 'react';
+import styles from './login.module.css';
+
+import { useDispatch } from 'react-redux';
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const Login = () => (
-  <>
-    <h1 className='text-center mt-3 text-white'>Login</h1>
+import useForm from '../../hooks/useForm';
 
-    <Form className='col-sm-10 offset-sm-1 mt-5'>
-      <Form.Group as={Row} controlId="formHorizontalEmail">
-        <Form.Label column sm={{span: 2, offset: 3}} className='text-white'>
-          Email
-        </Form.Label>
-        <Col sm={4}>
-          <Form.Control type="email" placeholder="Email" />
-        </Col>
-      </Form.Group>
+import { userActions } from '../../actions';
 
-      <Form.Group as={Row} controlId="formHorizontalPassword">
-        <Form.Label column sm={{span: 2, offset: 3}} className='text-white'>
-          Password
-        </Form.Label>
-        <Col sm={4}>
-          <Form.Control type="password" placeholder="Password" />
-        </Col>
-      </Form.Group>
+const Login = () => {
+  const dispatch = useDispatch();
 
-      <Form.Group as={Row} className='mt-5'>
-        <Col>
-          <Button className='col-sm-2 offset-sm-5' type="submit">Login</Button>
-        </Col>
-      </Form.Group>
-    </Form>
-  </>
-);
+  const stateSchema = {
+    username: { value: '', error: '' },
+    password: { value: '', error: '' }
+  };
+
+  const validationStateSchema = {
+    username: {
+      required: true,
+      validator: {
+        //regEx: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+        regEx: /.+/,
+        error: 'Username is required'
+      },
+    },
+    password: {
+      required: true,
+      validator: {
+        //regEx: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
+        regEx: /.+/,
+        error: 'Password is required'
+      },
+    }
+  };
+
+  const handleSubmit = (state) => {
+    dispatch(userActions.login(state.username.value, state.password.value));
+  };
+
+  const { state, handleOnChange, handleOnSubmit, disable } = useForm(
+    stateSchema,
+    validationStateSchema,
+    handleSubmit
+  );
+
+  return (
+    <div className={styles.container}>
+      <h1 className='text-center mt-3 text-white'>Login</h1>
+
+      <Form onSubmit={handleOnSubmit} className='col-sm-10 offset-sm-1 mt-5'>
+        <Form.Group as={Row} controlId="formHorizontalUsername">
+          <Form.Label column sm={{span: 2, offset: 3}} className='text-white'>
+            Username
+          </Form.Label>
+          <Col sm={4}>
+            <Form.Control
+              placeholder="Username"
+              name="username"
+              value={state.username.value}
+              onChange={handleOnChange}
+              required
+              isValid={state.username.value.length > 0}
+              isInvalid={state.username.value.length === 0}
+            />
+          </Col>
+          {state.username.error && <Form.Label column sm={{span: 4, offset: 5}} className='text-danger text-bold'>{state.username.error}</Form.Label>}
+        </Form.Group>
+
+        <Form.Group as={Row} controlId="formHorizontalPassword">
+          <Form.Label column sm={{span: 2, offset: 3}} className='text-white'>
+            Password
+          </Form.Label>
+          <Col sm={4}>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name='password'
+              value={state.password.value}
+              onChange={handleOnChange}
+              required
+              isValid={state.password.value.length > 0}
+              isInvalid={state.password.value.length === 0}
+            />
+          </Col>
+          {state.password.error && <Form.Label column sm={{span: 4, offset: 5}} className='text-danger text-bold'>{state.password.error}</Form.Label>}
+        </Form.Group>
+
+        <Form.Group as={Row} className='mt-5'>
+          <Col>
+            <Button className='col-sm-2 offset-sm-5' type="submit" disabled={disable}>Login</Button>
+          </Col>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+}
 
 export default Login;
