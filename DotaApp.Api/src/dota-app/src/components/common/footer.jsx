4fce9c340a5dd32fa963c445138bcd03da28f467
Georgi Stankov <vangog06406@gmail.com>
2019-08-19 21:30:00 +0300

    Added additional information to the footer

--- DotaApp.Api/src/dota-app/src/components/common/footer.jsx
+++ DotaApp.Api/src/dota-app/src/components/common/footer.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="bg-dark text-center text-white">
-    <p className='mb-0'>Copyright &copy; 2019 DotaApp Inc.</p>
+    <p className='mb-0'>
+      Made with <span role='img' aria-label='heart'>❤️</span>
+      by <a className='font-weight-bold font-italic' href='https://github.com/VanGog06' target='_blank' rel="noopener noreferrer">
+        <u>Georgi Stankov</u>
+      </a>
+    </p>
+
+    <p>
+      Copyright &copy; 2019 DotaApp Inc.</p>
   </footer>
 );
 
