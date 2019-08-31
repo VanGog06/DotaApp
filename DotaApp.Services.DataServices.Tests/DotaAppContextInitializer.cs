6f3d88219e4dab5ed451400248f58f076ef0c23c
Georgi Stankov <vangog06406@gmail.com>
2019-08-31 18:41:20 +0300

    Created tests project and static context initializer

---
+++ DotaApp.Services.DataServices.Tests/DotaAppContextInitializer.cs
@@ -0,0 +1,18 @@
+﻿using DotaApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DotaApp.Services.DataServices.Tests
+{
+    public static class DotaAppContextInitializer
+    {
+        public static DotaAppContext InitializeContext()
+        {
+            var options = new DbContextOptionsBuilder<DotaAppContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            return new DotaAppContext(options);
+        }
+    }
+}
