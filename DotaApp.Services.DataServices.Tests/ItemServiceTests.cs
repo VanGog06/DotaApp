656d5c314c18f3d1d2906f4c7bae507d016ffb22
Georgi Stankov <vangog06406@gmail.com>
2019-08-31 18:41:49 +0300

    Created Item and Team tests

---
+++ DotaApp.Services.DataServices.Tests/ItemServiceTests.cs
@@ -0,0 +1,79 @@
+﻿using DotaApp.Common;
+using DotaApp.Data;
+using DotaApp.Data.Common;
+using DotaApp.Data.DbModels;
+using DotaApp.Services.DataServices.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace DotaApp.Services.DataServices.Tests
+{
+    public class ItemServiceTests
+    {
+        private IItemService itemService;
+
+        private List<Item> GetDummyItems()
+        {
+            return new List<Item>
+            {
+                new Item { Id = 1, Cost = 1000, Image = "Image one", Lore = "First lore", Name = "Item one" },
+                new Item { Id = 2, Cost = 4000, Image = "Image two", Lore = "Second lore", Name = "Item two" },
+                new Item { Id = 3, Cost = 1234, Image = "Image three", Lore = "Third lore", Name = "Item three" }
+            };
+        }
+
+        private void SeedItems(DotaAppContext context)
+        {
+            context.Items.AddRange(this.GetDummyItems());
+            context.SaveChanges();
+        }
+
+        [Fact]
+        public void GetAll_WithDummyItems_ShoudlReturnCorrectResults()
+        {
+            var context = DotaAppContextInitializer.InitializeContext();
+            this.SeedItems(context);
+
+            this.itemService = new ItemService(context);
+
+            var expectedItems = context.Items.ToList();
+            var actualItems = this.itemService.GetAll();
+
+            Assert.Equal(expectedItems.Count, actualItems.Count);
+        }
+
+        [Fact]
+        public void GetById_WithDummyItems_ShouldThrowExceptionWhenItemNotFound()
+        {
+            var context = DotaAppContextInitializer.InitializeContext();
+            this.SeedItems(context);
+
+            this.itemService = new ItemService(context);
+
+            int itemId = 123;
+            DotaException exception = Assert.Throws<DotaException>(() => this.itemService.GetById(itemId));
+
+            Assert.Equal(Constants.InvalidOperation, exception.Message);
+        }
+
+        [Fact]
+        public void GetById_WithDummyItems_ShouldReturnCorrectResults()
+        {
+            var context = DotaAppContextInitializer.InitializeContext();
+            this.SeedItems(context);
+
+            this.itemService = new ItemService(context);
+
+            int itemId = 2;
+
+            var expectedItem = context.Items
+                .First(i => i.Id == itemId);
+
+            var actualItem = this.itemService.GetById(itemId);
+
+            Assert.Equal(expectedItem.Id, actualItem.Id);
+            Assert.Equal(expectedItem.Name, actualItem.Name);
+        }
+    }
+}
---
+++ DotaApp.Services.DataServices.Tests/TeamServiceTests.cs
@@ -0,0 +1,92 @@
+﻿using DotaApp.Data;
+using DotaApp.Data.DbModels;
+using DotaApp.Services.DataServices.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace DotaApp.Services.DataServices.Tests
+{
+    public class TeamServiceTests
+    {
+        private ITeamService teamsService;
+
+        private List<Team> GetDummyTeams()
+        {
+            return new List<Team>
+            {
+                new Team { Name = "First team", Rating = 1200, Wins = 300, Losses = 15, LogoUrl = "Some url", LastMatchTime = DateTime.UtcNow.ToLocalTime() },
+                new Team { Name = "Second team", Rating = 1500, Wins = 200, Losses = 150, LogoUrl = "Another url", LastMatchTime = DateTime.UtcNow.ToLocalTime() },
+                new Team { Name = "Third team", Rating = 800, Wins = 10, Losses = 50, LogoUrl = "Third url", LastMatchTime = DateTime.UtcNow.ToLocalTime() }
+            };
+        }
+
+        private List<Team> GetDummyTeamsWithEmptyNameAndLogoUrl()
+        {
+            return new List<Team>
+            {
+                new Team { Name = "First team", Rating = 1200, Wins = 300, Losses = 15, LogoUrl = "Some url", LastMatchTime = DateTime.UtcNow.ToLocalTime() },
+                new Team { Name = "Second team", Rating = 1500, Wins = 200, Losses = 150, LogoUrl = "Another url", LastMatchTime = DateTime.UtcNow.ToLocalTime() },
+                new Team { Name = "Third team", Rating = 800, Wins = 10, Losses = 50, LogoUrl = "Third url", LastMatchTime = DateTime.UtcNow.ToLocalTime() },
+                new Team { Name = "Fourth team", Rating = 100, Wins = 411, Losses = 68, LogoUrl = "", LastMatchTime = DateTime.UtcNow.ToLocalTime() },
+                new Team { Name = "", Rating = 1234, Wins = 710, Losses = 500, LogoUrl = "Fifth url", LastMatchTime = DateTime.UtcNow.ToLocalTime() }
+            };
+        }
+
+        private void SeedTeams(DotaAppContext context, List<Team> teams)
+        {
+            context.Teams.AddRange(teams);
+            context.SaveChanges();
+        }
+
+        [Fact]
+        public void All_WithDummyTeams_ShoudlReturnCorrectResults()
+        {
+            var context = DotaAppContextInitializer.InitializeContext();
+            this.SeedTeams(context, this.GetDummyTeams());
+
+            this.teamsService = new TeamService(context);
+
+            var expectedTeams = context.Teams.ToList();
+            var actualTeams = this.teamsService.All();
+
+            Assert.Equal(expectedTeams.Count, actualTeams.Count);
+        }
+
+        [Fact]
+        public void All_WithDummyTeamsWithEmptyNameAndLogoUrl_ShouldReturnCorrectResults()
+        {
+            var context = DotaAppContextInitializer.InitializeContext();
+            this.SeedTeams(context, this.GetDummyTeamsWithEmptyNameAndLogoUrl());
+
+            this.teamsService = new TeamService(context);
+
+            var expectedTeams = context.Teams
+                .Where(t => !string.IsNullOrEmpty(t.Name) && !string.IsNullOrEmpty(t.LogoUrl))
+                .ToList();
+
+
+            var actualTeams = this.teamsService.All();
+
+            Assert.Equal(expectedTeams.Count, actualTeams.Count);
+        }
+
+        [Fact]
+        public void All_WithDummyData_ShouldBeOrderedByRatingCorrectly()
+        {
+            var context = DotaAppContextInitializer.InitializeContext();
+            this.SeedTeams(context, this.GetDummyTeams());
+
+            this.teamsService = new TeamService(context);
+
+            var expectedTeams = context.Teams
+                .OrderByDescending(t => t.Rating)
+                .ToList();
+
+            var actualTeams = this.teamsService.All();
+
+            Assert.Equal(expectedTeams.First().Name, actualTeams.First().Name);
+        }
+    }
+}
